3a05af0fdf928e34d67f6abd8bb6b07cfff0f9e3
Federico Feroldi <fferoldi@gilt.com>
2013-02-04 12:05:38 -0500

    init

---
+++ extra/build_html.js
@@ -0,0 +1,7 @@
+// build styled page HTML
+$(document).ready(function() {
+  $('head').append('<link href="https://betadev.gilt.com/assets/stylesheets/bootstrap.min.css" rel="stylesheet" />');
+  var $html = '
+
+';
+});
