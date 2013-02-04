ff29d8e69a2e95ae0525c9fbc2035a8a0ed8e5fe
Federico Feroldi <fferoldi@gilt.com>
2013-02-04 12:19:15 -0500

    simple header

--- extra/build_html.js
+++ extra/build_html.js
@@ -4,5 +4,11 @@ $(document).ready(function() {
     '<link href="https://betadev.gilt.com/assets/stylesheets/bootstrap.min.css" rel="stylesheet" />' +
     '<link href="https://betadev.gilt.com/assets/stylesheets/custom.css" rel="stylesheet" />'
     );
-  var $html = '';
+  $('body').html(
+    '<div class="navbar navbar-inverse navbar-top">' +
+    '<div class="navbar-inner">' +
+    '<a class="brand" href="/"><img style="position: relative; top: -2px;" src="https://betadev.gilt.com/assets/img/gilt-logo-header.png"> <span>Developers</span></a>' +
+    '</div>' +
+    '</div>'
+    );
 });
