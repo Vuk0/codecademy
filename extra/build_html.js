4e6696f1f62620515cc9fd324f9030b04b6483db
Federico Feroldi <fferoldi@gilt.com>
2013-02-04 12:21:55 -0500

    codecademy logo

--- extra/build_html.js
+++ extra/build_html.js
@@ -7,7 +7,8 @@ $(document).ready(function() {
   $('body').html(
     '<div class="navbar navbar-inverse navbar-top">' +
     '<div class="navbar-inner">' +
-    '<a class="brand" href="/"><img style="position: relative; top: -2px;" src="https://betadev.gilt.com/assets/img/gilt-logo-header.png"> <span>Developers</span></a>' +
+    '<a class="brand" href="#"><img style="position: relative; top: -2px;" src="https://betadev.gilt.com/assets/img/gilt-logo-header.png"> <span>Developers</span></a>' +
+    ' for <img src="http://www.codecademy.com/assets/codecademy_logo_smallest-0323fdd409fc100a1991db8aa706f57e.png />' +
     '</div>' +
     '</div>'
     );
