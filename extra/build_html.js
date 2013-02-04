135b09b1b11f9b434c95c5ff7bc9757f4dd52f0f
Federico Feroldi <fferoldi@gilt.com>
2013-02-04 12:12:45 -0500

    basic css for the excercises

--- extra/build_html.js
+++ extra/build_html.js
@@ -1,7 +1,8 @@
 // build styled page HTML
 $(document).ready(function() {
-  $('head').append('<link href="https://betadev.gilt.com/assets/stylesheets/bootstrap.min.css" rel="stylesheet" />');
-  var $html = '
-
-';
+  $('head').append(
+    '<link href="https://betadev.gilt.com/assets/stylesheets/bootstrap.min.css" rel="stylesheet" />' +
+    '<link href="https://betadev.gilt.com/assets/stylesheets/custom.css" rel="stylesheet" />'
+    );
+  var $html = '';
 });
