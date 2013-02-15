9ab3cf47b308701ddc3eaecba7fe0702ea6065a5
Federico Feroldi <fferoldi@gilt.com>
2013-02-15 11:32:57 -0500

    update

--- data/active_products.js
+++ data/active_products.js
@@ -1,4 +1,4 @@
-$(window).trigger("products", [
+$(window).trigger("products", { products: [
   {
     "name": "Navajo Sweater",
     "product": "https://api.gilt.com/v1/products/177051229/detail.json",
@@ -91872,4 +91872,4 @@ $(window).trigger("products", [
       "Children"
     ]
   }
-]);
+]});
